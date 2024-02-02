7567c0797133c12330dcb04c41481c41243faa52
unaisek <mohammedunaiseknkt@gmail.com>
2024-02-02 09:30:11 +0530

    server url updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://elearnify-server-up3x.onrender.com',
   stripeKey:
     'pk_test_51OPibCSDFftmsmNtQ5VQieQ6UgtCRrxvxux2uBvG4MAMNcBsSHILVzL5iCZ9TQxkZmBhHdVEjUsgQUDvmftIodWS00Ankgwk7X',
 };
