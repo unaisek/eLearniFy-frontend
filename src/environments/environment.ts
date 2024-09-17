30693418c280d0e2e92591bb915d0dd2e3709e82
unaisek <mohammedunaiseknkt@gmail.com>
2024-09-17 19:54:29 +0530

    environment variable updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://footies.online/api',
+  apiUrl: 'https://elearnify.shop/api',
   // apiUrl: 'http://localhost:3000/api',
   stripeKey:
     'pk_test_51OPibCSDFftmsmNtQ5VQieQ6UgtCRrxvxux2uBvG4MAMNcBsSHILVzL5iCZ9TQxkZmBhHdVEjUsgQUDvmftIodWS00Ankgwk7X',
